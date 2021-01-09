85a8d06a4ea34881bfb5fc83fe77979b5decd268
James Muriithi <muriithijames556@gmail.com>
2021-01-09 15:07:07 +0300

    changed schedule hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('email:send')->twiceDaily(1,13);
+         $schedule->command('email:send')->twiceDaily(1,17);
     }
 
     /**
